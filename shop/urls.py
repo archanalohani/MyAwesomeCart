c0d71d31b06f37c36befa36f4eb88daffb52ad9e
archanalohani <archanalohani42@gmail.com>
2021-01-30 20:44:10 +0530

    shop/about,about page inside shop folder

--- shop/urls.py
+++ shop/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from.import views
 
 urlpatterns = [  path("",views.index,name="ShopHome"),
-                 path("about/",views.index,name="About us"),
-                 path("contact/",views.index,name="contact us"),
-                 path("tracker/",views.index,name="tracker"),
-                 path("search/",views.index,name="search"),
-                 path("productview/",views.index,name="productview"),
-                 path("checkout/",views.index,name="checkout"),
+                 path("about/",views.about,name="About us"),
+                 path("contact/",views.contact,name="contact us"),
+                 path("tracker/",views.tracker,name="tracker"),
+                 path("search/",views.search,name="search"),
+                 path("productview/",views.productView,name="productview"),
+                 path("checkout/",views.checkout,name="checkout"),
 
 ] 
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
     return render(request,'shop/index.html')
+    
+    
+def about(request):
+    return render(request,'shop/about.html')
+
+def contact(request):
+    return HttpResponse("we are contact")
+
+def tracker(request):
+    return HttpResponse("we are tracker")
+    
+def search(request):
+    return HttpResponse("we are search")
+
+def productView(request):
+    return HttpResponse("we are productview")
+
+def checkout(request):
+    return HttpResponse("we are checkout")
